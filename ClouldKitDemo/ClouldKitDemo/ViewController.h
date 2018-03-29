a6c2f6609b56a8ac36811008b08d7ec852410a8c
杨冬武 <apple@DWdeiMac.local>
2018-03-29 11:25:59 +0800

    初始化项目

---
+++ ClouldKitDemo/ClouldKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClouldKitDemo
+//
+//  Created by Apple on 2018/3/29.
+//  Copyright © 2018年 Dongwu Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ClouldKitDemo/ClouldKitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ClouldKitDemo
+//
+//  Created by Apple on 2018/3/29.
+//  Copyright © 2018年 Dongwu Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
