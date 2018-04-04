81b051adbaaafb64dd0002b235c49302991c1f56
杨冬武 <apple@DWdeiMac.local>
2018-04-04 16:15:43 +0800

    save record

--- ClouldKitDemo/ClouldKitDemo/ViewController.m
+++ ClouldKitDemo/ClouldKitDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <CloudKit/CloudKit.h>
 
 @interface ViewController ()
 
@@ -16,7 +17,64 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [[CKContainer defaultContainer] accountStatusWithCompletionHandler:^(CKAccountStatus accountStatus, NSError *error) {
+        
+        if (accountStatus == CKAccountStatusNoAccount) {
+            
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Sign in to iCloud"
+                                        
+                                                                           message:@"Sign in to your iCloud account to write records. On the Home screen, launch Settings, tap iCloud, and enter your Apple ID. Turn iCloud Drive on. If you don't have an iCloud account, tap Create a new Apple ID."
+                                        
+                                                                    preferredStyle:UIAlertControllerStyleAlert];
+            
+            [alert addAction:[UIAlertAction actionWithTitle:@"Okay"
+                              
+                                                      style:UIAlertActionStyleCancel
+                              
+                                                    handler:nil]];
+            
+            [self presentViewController:alert animated:YES completion:nil];
+            
+        }
+        
+        else {
+            
+            // Insert your just-in-time schema code here
+            CKRecordID *artworkRecordID = [[CKRecordID alloc] initWithRecordName:@"115"];
+            
+            CKRecord *artworkRecord = [[CKRecord alloc] initWithRecordType:@"ArtWork" recordID:artworkRecordID];
+            
+            artworkRecord[@"title" ] = @"MacKerricher State Park";
+            
+            artworkRecord[@"artist"] = @"Mei Chen";
+            
+            artworkRecord[@"address"] = @"Fort Bragg, CA";
+            
+            CKContainer *myContainer = [CKContainer defaultContainer];
+            
+            //    CKDatabase *privateDatabase = [myContainer privateCloudDatabase];
+            CKDatabase *publicDatabase = [myContainer publicCloudDatabase];
+            
+            [publicDatabase saveRecord:artworkRecord completionHandler:^(CKRecord * _Nullable record, NSError * _Nullable error) {
+                
+                if (error) {
+                    NSLog(@"failed,%@",error);
+                    return;
+                }
+                
+                NSLog(@"success");
+                
+            }];
+            
+        }
+        
+    }];
+    
+    
+    
+    
 }
 
 
