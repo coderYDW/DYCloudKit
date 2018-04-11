0e32adfc1284a5f55c1c1e7f6873e84f350e265d
杨冬武 <apple@DWdeiMac.local>
2018-04-11 21:05:39 +0800

    reference
    fetch with reference

--- ClouldKitDemo/ClouldKitDemo/FetchRecord/DYFetchRecordViewController.m
+++ ClouldKitDemo/ClouldKitDemo/FetchRecord/DYFetchRecordViewController.m
@@ -26,36 +26,88 @@
 
 - (IBAction)fetchAction:(id)sender {
     
-    CKDatabase *publicDatabase = [[CKContainer defaultContainer] publicCloudDatabase];
-
-    //根据条件查询
-//    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"artist = %@", @"Mei Chen"];
     
-    //根据位置查询记录
-    CLLocation *fixedLocation = [[CLLocation alloc] initWithLatitude:22.547 longitude:114.085947];
-    CGFloat radius = 100000; // meters
-    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"distanceToLocation:fromLocation:(location, %@) < %f",fixedLocation,radius];
+    [self fetchRecordWithReference];
+}
 
-    CKQuery *query = [[CKQuery alloc] initWithRecordType:@"Artwork" predicate:predicate];
 
-    [publicDatabase performQuery:query inZoneWithID:nil completionHandler:^(NSArray<CKRecord *> * _Nullable results, NSError * _Nullable error) {
+- (void)fetchRecordWithReference {
+    
+    CKDatabase *publicDatabase = [[CKContainer defaultContainer] publicCloudDatabase];
+    CKRecordID *recordID = [[CKRecordID alloc] initWithRecordName:@"115"];
+    [publicDatabase fetchRecordWithID:recordID completionHandler:^(CKRecord * _Nullable record, NSError * _Nullable error) {
         if (error) {
             NSLog(@"%@",error);
             return;
         }
-
-        NSLog(@"perform query success : %@", results);
-
-        [self.records addObjectsFromArray:results];
-
-        [self.tableView reloadData];
+        NSLog(@"%@",record);
+        
+        [self.records addObjectsFromArray:@[record]];
+        
+        CKReference *reference = [record objectForKey:@"artist1"];
+        
+        [publicDatabase fetchRecordWithID:reference.recordID completionHandler:^(CKRecord * _Nullable record, NSError * _Nullable error) {
+            if (error) {
+                NSLog(@"%@",error);
+                return;
+            }
+            NSLog(@"%@",record);
+            
+            [self.records addObjectsFromArray:@[record]];
+            
+            [self.tableView reloadData];
+        }];
         
     }];
     
 }
 
+- (void)fetchRecordWithLocation {
+    CKDatabase *publicDatabase = [[CKContainer defaultContainer] publicCloudDatabase];
+    //根据位置查询记录
+    CLLocation *fixedLocation = [[CLLocation alloc] initWithLatitude:22.547 longitude:114.085947];
+    CGFloat radius = 100000; // meters
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"distanceToLocation:fromLocation:(location, %@) < %f",fixedLocation,radius];
+    
+    CKQuery *query = [[CKQuery alloc] initWithRecordType:@"Artwork" predicate:predicate];
+    
+    [publicDatabase performQuery:query inZoneWithID:nil completionHandler:^(NSArray<CKRecord *> * _Nullable results, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"%@",error);
+            return;
+        }
+        
+        NSLog(@"perform query success : %@", results);
+        
+        [self.records addObjectsFromArray:results];
+        
+        [self.tableView reloadData];
+        
+    }];
+}
 
-
+- (void)fetchRecordWithPredicate {
+    CKDatabase *publicDatabase = [[CKContainer defaultContainer] publicCloudDatabase];
+    
+    //根据条件查询
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"artist = %@", @"Mei Chen"];
+    
+    CKQuery *query = [[CKQuery alloc] initWithRecordType:@"Artwork" predicate:predicate];
+    
+    [publicDatabase performQuery:query inZoneWithID:nil completionHandler:^(NSArray<CKRecord *> * _Nullable results, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"%@",error);
+            return;
+        }
+        
+        NSLog(@"perform query success : %@", results);
+        
+        [self.records addObjectsFromArray:results];
+        
+        [self.tableView reloadData];
+        
+    }];
+}
 
 
 - (void)fetchAndEditRecord {
@@ -105,9 +157,14 @@
     
     CKRecord *artwork = self.records[indexPath.row];
     
-    cell.textLabel.text = [artwork objectForKey:@"title"];
-    
-    cell.detailTextLabel.text = [artwork objectForKey:@"address"];
+    if ([artwork.recordType isEqualToString:@"ArtWork"]) {
+        cell.textLabel.text = [artwork objectForKey:@"title"];
+        cell.detailTextLabel.text = [artwork objectForKey:@"address"];
+    }
+    else {
+        cell.textLabel.text = [artwork objectForKey:@"firstName"];
+        cell.detailTextLabel.text = [artwork objectForKey:@"lastName"];
+    }
     
     return cell;
 }
